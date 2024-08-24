f2a96c48313d52569ab8740e717ab0845e00f7b5
Rizki Januar I. <id.rizkijanuar@gmail.com>
2024-08-25 00:28:10 +0700

    🚨 test : testing transaction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Illuminate\Database\Eloquent\Model::unguard();
+        // Blade::component('components.index', 'table');          // INDEX
+        // Blade::component('components.thead', 'table.thead');   // Untuk thead
+        // Blade::component('components.tr', 'table.tr');         // Untuk tr
+        // Blade::component('components.th', 'table.th');         // Untuk th
+        // Blade::component('components.td', 'table.td');         // Untuk td
     }
 }
